52ea8b6fe9d8ce8b3fae587afdebc822d7078041
od <xzdheogus1@vanillacode.com>
2024-06-27 10:04:22 +0900

    update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
-// @Module({
-//   imports: [UsersModule],
-//   controllers: [AppController, UsersController],
-//   providers: [AppService],
-// })
-
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
